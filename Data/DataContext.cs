2106fda218164ed1207a27a02406b9652181e516
Darren Tuggey <darren.tuggey@outlook.com>
2020-02-02 19:51:39 -0800

    teset

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingAppApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
